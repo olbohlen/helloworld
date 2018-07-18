7d0a7a1de1d710ab59286c6f91c98dcb1463d96b
Olaf Bohlen <olaf.bohlen@niit.com>
2018-07-18 15:48:57 +0200

    demo for cm and secret

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
   <title>hello php</title>
  </head>
  <body>
- <?php echo '<p>hello world</p>'; ?>
+   <?php 
+    echo '<p>hello world from ' .$_ENV["HOSTNAME"] . '</p>';
+    echo '<p>TEXT = ' .$_ENV["TEXT"] . '</p>';
+    echo '<p>PASSWORD = ' .$_ENV["PASSWORD"] . '</p>';
+    $myFile = "/geheim/passwordfile";
+    if(file_exists($myFile)) {
+      readfile($MyFile);
+    }
+?>
  </body>
 </html>
 
