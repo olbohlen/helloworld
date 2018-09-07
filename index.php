82b485a5da36a3bad58192489916011b1fde7f4d
Olaf Bohlen <olaf.bohlen@niit.com>
2018-09-07 09:04:37 +0200

    foo

--- index.php
+++ index.php
@@ -7,10 +7,6 @@
     echo '<p>hello world from ' .$_ENV["HOSTNAME"] . '</p>';
     echo '<p>TEXT = ' .$_ENV["TEXT"] . '</p>';
     echo '<p>PASSWORD = ' .$_ENV["PASSWORD"] . '</p>';
-    $myFile = "/geheim/passwordfile";
-    if(file_exists($myFile)) {
-      readfile($MyFile);
-    }
 ?>
  </body>
 </html>
